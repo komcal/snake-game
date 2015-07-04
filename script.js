b951a2a0e96af01fb0cc4b45732f979a685dd929
Komkanit <kom.sujautra@gmail.com>
2015-07-05 01:05:48 +0700

    add map

--- script.js
+++ script.js
@@ -9,32 +9,60 @@ var game = true,
 	n: 3,
 	size: 10,
 	order:["r","r","r"]
-	};
-
-$(document).ready(function(){
-	render();
+	},
+	item = [{
+		x: [],
+		y: [],
+		n: 0,
+		color: "#CD0000"},
+		{
+		x: [],
+		y: [],
+		n: 0,
+		color: "#104E8B"},
+		{
+		x: [],
+		y: [],
+		n: 0,
+		color: "#EEEE00"}],
+	IntervalId;
 	
-	setInterval(function() {
+var map = new Array(40);
+$(document).ready(function(){
+	
+  for (var i = 0; i < 40; i++) {
+    map[i] = new Array(40);
+    for(var j = 0 ; j < 40 ; j++){
+			map[i][j] = 0;
+		}
+  }
+	
+	render();
+
+	IntervalId = setInterval(function() {
    		update();
    		render();
-
-	}, 4000/60);
+	}, 5000/60);
 
 	$(document).keydown(function(e) {
     switch(e.which) {
         case 37: // left
+        if(key != "r")
         key = "l";
         break;
 
         case 38: // up
+        if(key != "d")
         key = "u";
         break;
 
         case 39: // right
+        if(key != "l")
         key ="r";
         break;
 
         case 40: // down
+        if(key != "u")
         key = "d";
         break;
 
@@ -47,6 +75,10 @@ $(document).ready(function(){
 function update(){
 	canvas.width = canvas.width;
 	
+	for(var i = 0 ; i < snake.n ; i++){
+		if(snake.y[i] >= 0 && snake.y[i]<=560 && snake.x[i] >= 0 && snake.x[i]<=560)
+		map[snake.y[i]/pixel][snake.x[i]/pixel] = 0;
+	}	
 	for(var i = 0 ; i < snake.n ; i++){
 		if(i < snake.n-1){
 			if(snake.order[i+1]=="r"){snake.x[i]+=pixel;}
@@ -61,17 +93,24 @@ function update(){
 			else if(snake.order[i]=="u"){snake.y[i]-=pixel;}
 			else if(snake.order[i]=="l"){snake.x[i]-=pixel;}
 			else if(snake.order[i]=="d"){snake.y[i]+=pixel;}
+			//console.log("**" + snake.y[i]);
+			
 		}
-
+		if(snake.y[i] >= 0 && snake.y[i]<560 && snake.x[i] >= 0 && snake.x[i]<=560){
+			map[snake.y[i]/pixel][snake.x[i]/pixel] = 1;
+				
+		}
 	}
-	
 }
 
 function render(){
-	if(snake.x[snake.n-1] < 0 || snake.x[snake.n-1] >= 560)game = false;
-	if(snake.y[snake.n-1] < 0 || snake.y[snake.n-1] >= 560)game = false;
+	//console.log(snake.y[snake.n-1]);
+	if(snake.y[snake.n-1] < 0 || snake.y[snake.n-1] >= 560){game = false;console.log("end");}
+	if(snake.x[snake.n-1] < 0 || snake.x[snake.n-1] >= 560){game = false;console.log("end");}
+	
 
 	if(game)createDotSnake();
+	else clearInterval(IntervalId);
 }
 
 function createDotSnake(){
@@ -80,5 +119,12 @@ function createDotSnake(){
 		ctx.fillStyle = "green";
 		ctx.fillRect(snake.x[i],snake.y[i],snake.size,snake.size);
 		
+	}
+}
+function spawn(){
+	for(var i = 0 ; i < 2 ; i++){
+		item[i].n++;
+		
+
 	}
 }
